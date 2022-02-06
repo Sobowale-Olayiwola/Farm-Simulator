94ed0b2fcf708ab29a05eb319f2dcd23f492055d
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2022-02-06 18:11:46 +0100

    Updated models to create associations

--- src/models/farmbuilding.js
+++ src/models/farmbuilding.js
@@ -1,11 +1,20 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const FarmBuilding = sequelize.define('FarmBuilding', {
-    unit_type: DataTypes.STRING,
-    name: DataTypes.STRING
-  }, {});
-  FarmBuilding.associate = function(models) {
+  const Farm_Building = sequelize.define(
+    "Farm_Building",
+    {
+      unit_type: DataTypes.STRING,
+      next_feeding_time: DataTypes.DATE,
+      email: DataTypes.STRING,
+    },
+    {}
+  );
+  Farm_Building.associate = function (models) {
     // associations can be defined here
+    Farm_Building.hasMany(models.Farm_Unit, {
+      foreignKey: "farm_building_id",
+      as: "farm_unit",
+      onDelete: "CASCADE",
+    });
   };
-  return FarmBuilding;
+  return Farm_Building;
 };
--- src/models/farmunit.js
+++ src/models/farmunit.js
@@ -1,13 +1,21 @@
-'use strict';
 module.exports = (sequelize, DataTypes) => {
-  const FarmUnit = sequelize.define('FarmUnit', {
-    last_time_fed: DataTypes.INTEGER,
-    alive: DataTypes.BOOLEAN,
-    farm_building_id: DataTypes.INTEGER,
-    health_point: DataTypes.INTEGER
-  }, {});
-  FarmUnit.associate = function(models) {
+  const Farm_Unit = sequelize.define(
+    "Farm_Unit",
+    {
+      next_feeding_time: DataTypes.DATE,
+      alive: DataTypes.BOOLEAN,
+      farm_building_id: DataTypes.INTEGER,
+      health_point: DataTypes.INTEGER,
+    },
+    {}
+  );
+  Farm_Unit.associate = function (models) {
     // associations can be defined here
+    Farm_Unit.belongsTo(models.Farm_Building, {
+      foreignKey: "farm_building_id",
+      as: "farm_building",
+      onDelete: "CASCADE",
+    });
   };
-  return FarmUnit;
+  return Farm_Unit;
 };

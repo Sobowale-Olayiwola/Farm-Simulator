2b7b9f0040f753ff86cefd1680f4315c343cefd0
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2022-02-06 23:00:06 +0100

    Added middleware to rate limit for 5 secs

--- src/routes/farmUnit.js
+++ src/routes/farmUnit.js
@@ -1,5 +1,24 @@
 import express from "express";
 import * as farmUnitController from "../controllers/farmUnit/farmUnit.controller";
+import TokenBucket from "../helpers/tokenBucket";
+
+function limitRequests(perSecond, maxBurst) {
+  const buckets = new Map();
+
+  // Return an Express middleware function
+  return function limitRequestsMiddleware(req, res, next) {
+    if (!buckets.has(req.ip)) {
+      buckets.set(req.ip, new TokenBucket(maxBurst, perSecond));
+    }
+
+    const bucketForIP = buckets.get(req.ip);
+    if (bucketForIP.take()) {
+      next();
+    } else {
+      res.status(429).send("Client rate limit exceeded");
+    }
+  };
+}
 
 const router = express.Router();
 router.get(
@@ -7,6 +26,10 @@ router.get(
   farmUnitController.getFarmBuildingUnitHealthRecords
 );
 router.post("/farm-units", farmUnitController.createFarmUnit);
-router.put("/farm-units/:id", farmUnitController.updateFarmUnitHealth);
+router.put(
+  "/farm-units/:id",
+  limitRequests(1, 0.5),
+  farmUnitController.updateFarmUnitHealth
+);
 
 module.exports = router;

6c8560cf9ed2244f5ef101e5ff75c88ec5e0033c
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2022-02-06 18:07:23 +0100

    Updated FarmBuilding seed file

--- src/seeders/20220206170407-FarmBuilding.js
+++ src/seeders/20220206170407-FarmBuilding.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
@@ -12,6 +12,54 @@ module.exports = {
         isBetaMember: false
       }], {});
     */
+    return queryInterface.bulkInsert(
+      "FarmBuildings",
+      [
+        {
+          unit_type: "Pig",
+          name: "Barney's Pig Farm",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          unit_type: "Chicken",
+          name: "Meadlow's Chicken Farm",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          unit_type: "Sheep",
+          name: "Craig's Sheep Farm",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          unit_type: "Turkey",
+          name: "Sandy's Turkey Farm",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          unit_type: "Duck",
+          name: "Bill's Duck Farm",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          unit_type: "Cow",
+          name: "Sally's Cow Farm",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
+          unit_type: "Goat",
+          name: "Kentucky's Goat Farm",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {}
+    );
   },
 
   down: (queryInterface, Sequelize) => {
@@ -22,5 +70,6 @@ module.exports = {
       Example:
       return queryInterface.bulkDelete('People', null, {});
     */
-  }
+    return queryInterface.bulkDelete("FarmBuildings", null, {});
+  },
 };
